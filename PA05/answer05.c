abc7468459d80db22989b1361b21f2cadf034306
Adam Ribblett <aribblet@purdue.edu>
2014-02-21 01:44:49 -0500

    Wrote the linear Normaliza

--- PA05/answer05.c
+++ PA05/answer05.c
@@ -15,4 +15,21 @@ int Image_save(const char * filename, Image * image);
 
 void Image_free(Image * image);
 
-void linearNormalization(int width, int height, uint8_t * intensity);
+void linearNormalization(int width, int height, uint8_t * intensity)
+{
+  int ct; //Counter
+  int max = 0;
+  int min = 255;
+  for(ct = 0; ct < width * height; ct++)
+    {
+      if(intensity[ct] > max){
+	max = intensity[ct]; }
+      if(intensity[ct] < min){
+	min = intensity[ct]; }
+    }
+  for(ct = 0; ct < width * height; ct++)
+    {
+      intensity[ct] = (intensity - min) * 255 / (max - min);
+    }
+}
+
